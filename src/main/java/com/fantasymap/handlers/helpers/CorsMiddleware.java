5c37cffe3fbb729abf174c44e62ea9b7517683c1
Miško Dvořák <michal.dvorak999@gmail.com>
2024-01-07 18:05:17 +0100

    CORS: Add handling of preflight requests.

--- src/main/java/com/fantasymap/handlers/helpers/CorsMiddleware.java
+++ src/main/java/com/fantasymap/handlers/helpers/CorsMiddleware.java
@@ -12,9 +12,17 @@ public class CorsMiddleware extends BaseMiddleware{
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        if (exchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            exchange.sendResponseHeaders(204, -1);
+            return;
+        }
         exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
         exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "GET, OPTIONS");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "Content-Type");
+        exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        System.out.println("Cors allowed.");
         super.handle(exchange);
     }
 }
